13be176eb61ac6de447c7cf3ed115e8215cc9896
Jean-Sebastien Carle <29762210+jscarle@users.noreply.github.com>
2022-08-22 15:18:14 -0400

    Changed AccountDetails to init.

--- OnePassword.NET/Accounts/AccountDetails.cs
+++ OnePassword.NET/Accounts/AccountDetails.cs
@@ -5,20 +5,20 @@ namespace OnePassword.Accounts;
 public record AccountDetails
 {
     [JsonPropertyName("id")]
-    public string Id { get; set; } = "";
+    public string Id { get; init; } = "";
 
     [JsonPropertyName("name")]
-    public string Name { get; set; } = "";
+    public string Name { get; init; } = "";
 
     [JsonPropertyName("domain")]
-    public string Domain { get; set; } = "";
+    public string Domain { get; init; } = "";
 
     [JsonPropertyName("type")]
-    public AccountType AccountType { get; set; }
+    public AccountType AccountType { get; init; }
 
     [JsonPropertyName("state")]
-    public State State { get; set; }
+    public State State { get; init; }
 
     [JsonPropertyName("created_at")]
-    public DateTimeOffset CreatedAt { get; set; }
+    public DateTimeOffset CreatedAt { get; init; }
 }
